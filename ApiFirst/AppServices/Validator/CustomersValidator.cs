e6738bbb34e17b87cb5c6d15d02ee2bf186c0c61
JoaoArrudaWarren <joao.arruda@warren.com.br>
2022-09-20 11:29:39 -0300

    [fix] Fixing Indentation

--- ApiFirst/AppServices/Validator/CustomersValidator.cs
+++ ApiFirst/AppServices/Validator/CustomersValidator.cs
@@ -36,11 +36,7 @@ namespace AppServices.Validation
 
                 input = input.Trim();
                 char chars = input[0];
-                if (Char.IsUpper(chars))
-                {
-                    return true;
-                }
-                return false;
+                return Char.IsUpper(chars);
             }
 
             bool BeAValidCpf(string cpf)
