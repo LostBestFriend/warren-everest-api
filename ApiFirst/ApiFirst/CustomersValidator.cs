b05a6408d1c0363e043e67ba6f5f033ceeeda8f0
JoaoArrudaWarren <joao.arruda@warren.com.br>
2022-09-20 11:30:49 -0300

    [fix] Fixing Indentation

--- ApiFirst/ApiFirst/CustomersValidator.cs
+++ ApiFirst/ApiFirst/CustomersValidator.cs
@@ -29,69 +29,71 @@ namespace ApiFirst
             RuleFor(x => x.Address).NotEmpty().NotEqual(" ").MinimumLength(3).MaximumLength(200).Must(FirstLetterUpperCase).WithMessage("Please enter a valid Address");
 
             RuleFor(x => x.Number).NotEmpty().GreaterThanOrEqualTo(1);
-        }
-        bool FirstLetterUpperCase(string input)
-        {
-            input = input.Trim();
-            char chars = input[0];
-            return Char.IsUpper(chars);
-        }
 
-        bool BeAValidCpf(string cpf)
-        {
-
-            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-            string tempCpf;
-            string digito;
-            int soma;
-            int resto;
-
-            if (cpf.Length != 11) return false;
-            if (cpf.All(x => x == cpf.First())) return false;
-
-
-            tempCpf = cpf.Substring(0, 9);
-            soma = 0;
-
-            for (int i = 0; i < 9; i++)
+            bool FirstLetterUpperCase(string input)
             {
-                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+                input = input.Trim();
+                char chars = input[0];
+                return Char.IsUpper(chars);
             }
 
-            resto = soma % 11;
-            if (resto < 2)
+            bool BeAValidCpf(string cpf)
             {
-                resto = 0;
-            }
-            else
-            {
-                resto = 11 - resto;
-            }
-            digito = resto.ToString();
 
-            tempCpf = tempCpf + digito;
+                int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+                int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+                string tempCpf;
+                string digito;
+                int soma;
+                int resto;
 
-            soma = 0;
-            for (int i = 0; i < 10; i++)
-            {
-                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+                if (cpf.Length != 11) return false;
+                if (cpf.All(x => x == cpf.First())) return false;
+
+
+                tempCpf = cpf.Substring(0, 9);
+                soma = 0;
+
+                for (int i = 0; i < 9; i++)
+                {
+                    soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+                }
+
+                resto = soma % 11;
+                if (resto < 2)
+                {
+                    resto = 0;
+                }
+                else
+                {
+                    resto = 11 - resto;
+                }
+                digito = resto.ToString();
+
+                tempCpf = tempCpf + digito;
+
+                soma = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+                }
+
+                resto = soma % 11;
+                if (resto < 2)
+                {
+                    resto = 0;
+                }
+                else
+                {
+                    resto = 11 - resto;
+                }
+
+                digito = digito + resto.ToString();
+
+                return cpf.EndsWith(digito);
             }
-
-            resto = soma % 11;
-            if (resto < 2)
-            {
-                resto = 0;
-            }
-            else
-            {
-                resto = 11 - resto;
-            }
-
-            digito = digito + resto.ToString();
-
-            return cpf.EndsWith(digito);
         }
     }
 }
-}
+
+
