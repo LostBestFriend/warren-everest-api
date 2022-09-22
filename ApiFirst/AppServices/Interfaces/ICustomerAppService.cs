61bf27812f59bf8d5d95fc5faac8e7a81d034ac2
JoaoArrudaWarren <joao.arruda@warren.com.br>
2022-09-22 15:33:05 -0300

    [fix] blankspaces

--- ApiFirst/AppServices/Interfaces/ICustomerAppService.cs
+++ ApiFirst/AppServices/Interfaces/ICustomerAppService.cs
@@ -5,17 +5,11 @@ namespace AppServices.Interfaces
     public interface ICustomerAppService
     {
         long Create(CustomerCreateDTO model);
-
         void Update(int id, CustomerUpdateDTO model);
-
         void Delete(int id);
-
         List<CustomerResponseDTO> GetAll();
-
         CustomerResponseDTO GetById(int id);
-
         void Modify(int id, string email);
-
         CustomerResponseDTO GetByCpf(string cpf);
     }
 }
--- ApiFirst/AppServices/Services/CostumerAppService.cs
+++ ApiFirst/AppServices/Services/CostumerAppService.cs
@@ -13,23 +13,27 @@ namespace AppServices.Services
 
         public CustomerAppServices(ICustomerService customerServices, IMapper mapper)
         {
-            _customerServices = customerServices;
-            _mapper = mapper;
+            _customerServices = customerServices ?? throw new ArgumentNullException(nameof(customerServices));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
+
         public CustomerResponseDTO GetByCpf(string cpf)
         {
             var result = _customerServices.GetByCpf(cpf);
             return _mapper.Map<CustomerResponseDTO>(result);
         }
+
         public long Create(CustomerCreateDTO model)
         {
             var mapped = _mapper.Map<Customer>(model);
             return _customerServices.Create(mapped).Id;
         }
+
         public void Delete(int id)
         {
             _customerServices.Delete(id);
         }
+
         public List<CustomerResponseDTO> GetAll()
         {
             var result = _customerServices.GetAll();
@@ -41,11 +45,13 @@ namespace AppServices.Services
             var result = _customerServices.GetById(id);
             return _mapper.Map<CustomerResponseDTO>(result);
         }
+
         public void Update(int id, CustomerUpdateDTO model)
         {
             var mapped = _mapper.Map<Customer>(model);
             _customerServices.Update(id, mapped);
         }
+
         public void Modify(int id, string email)
         {
             _customerServices.Modify(id, email);
