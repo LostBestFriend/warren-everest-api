228117880a46bbaee0eba2e990d9cfe8681e5142
JoaoArrudaWarren <joao.arruda@warren.com.br>
2022-09-22 15:44:12 -0300

    [fix] blankspaces

--- ApiFirst/DomainServices/Interfaces/ICustomerService.cs
+++ ApiFirst/DomainServices/Interfaces/ICustomerService.cs
@@ -5,17 +5,11 @@ namespace DomainServices.Interfaces
     public interface ICustomerService
     {
         long Create(Customer model);
-
         void Update(Customer model);
-
         void Delete(int id);
-
         List<Customer> GetAll();
-
         Customer GetById(int id);
-
         void Modify(Customer model);
-
         Customer GetByCpf(string cpf);
     }
 }
