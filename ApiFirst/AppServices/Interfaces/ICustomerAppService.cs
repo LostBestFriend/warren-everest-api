2ac0f942a08534dbe6646154cbc6c581dae280f2
JoaoArrudaWarren <joao.arruda@warren.com.br>
2022-09-22 15:46:37 -0300

    [fix] blankspaces

--- ApiFirst/AppServices/Interfaces/ICustomerAppService.cs
+++ ApiFirst/AppServices/Interfaces/ICustomerAppService.cs
@@ -5,17 +5,11 @@ namespace AppServices.Interfaces
     public interface ICustomerAppService
     {
         Task<long> CreateAsync(CustomerCreateDTO model);
-
         void Update(int id, CustomerUpdateDTO model);
-
         void Delete(int id);
-
         IEnumerable<CustomerResponseDTO> GetAll();
-
         Task<CustomerResponseDTO> GetByIdAsync(int id);
-
         void Modify(int id, CustomerUpdateDTO model);
-
         Task<CustomerResponseDTO> GetByCpfAsync(string cpf);
     }
 }
--- ApiFirst/DomainServices/Interfaces/ICustomerService.cs
+++ ApiFirst/DomainServices/Interfaces/ICustomerService.cs
@@ -5,17 +5,11 @@ namespace DomainServices.Interfaces
     public interface ICustomerService
     {
         Task<long> CreateAsync(Customer model);
-
         void Update(Customer model);
-
         void Delete(int id);
-
         IEnumerable<Customer> GetAll();
-
         Task<Customer> GetByIdAsync(int id);
-
         void Modify(Customer model);
-
         Task<Customer> GetByCpfAsync(string cpf);
     }
 }
