5b8aad8009be8a010cb178859201d0cecf280c67
JoaoArrudaWarren <joao.arruda@warren.com.br>
2022-09-22 09:22:58 -0300

    [feat] fixing program and other things

--- ApiFirst/ApiFirst/Controllers/CustomersController.cs
+++ ApiFirst/ApiFirst/Controllers/CustomersController.cs
@@ -82,7 +82,7 @@ namespace WebApi.Controllers
             try
             {
                 _customerAppServices.Delete(id);
-                return Ok();
+                return NoContent();
             }
             catch (ArgumentNullException e)
             {
