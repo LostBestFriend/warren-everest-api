7dbe1bb15b1d3d6fad1d9612c0903809dfdb6d56
JoaoArrudaWarren <108344808+JoaoArrudaWarren@users.noreply.github.com>
2022-10-04 20:16:24 -0300

    Update CustomerService.cs

--- ApiFirst/DomainServices/Services/CustomerService.cs
+++ ApiFirst/DomainServices/Services/CustomerService.cs
@@ -1,5 +1,7 @@
-﻿using DomainModels.Models;
+using DomainModels.Models;
 using DomainServices.Interfaces;
+using Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,24 +10,30 @@ namespace DomainServices.Services
 {
     public class CustomerService : ICustomerService
     {
-        private readonly List<Customer> _customers = new();
+        private readonly WarrenContext _context;
+        private readonly DbSet<Customer> _customers;
 
-        public Customer Create(Customer model)
+        public CustomerService(WarrenContext context)
         {
-            model.Id = _customers.LastOrDefault()?.Id + 1 ?? 1;
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _customers = _context.Set<Customer>();
+        }
 
+        public long Create(Customer model)
+        {
             if (_customers.Any(customer => customer.Cpf == model.Cpf)) throw new ArgumentException($"Já existe usuário com o CPF {model.Cpf}");
             if (_customers.Any(customer => customer.Email == model.Email)) throw new ArgumentException($"Já existe usuário com o CPF {model.Email}");
 
             _customers.Add(model);
-            return model;
+            _context.SaveChanges();
+            return model.Id;
         }
 
         public void Delete(long id)
         {
             Customer customer = GetById(id);
 
-            _customers.Remove(customer);
+            _customers.Remove(customer).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
         }
 
         public IEnumerable<Customer> GetAll()
@@ -35,7 +43,6 @@ namespace DomainServices.Services
 
         public Customer GetByCpf(string cpf)
         {
-            cpf = cpf.Trim().Replace(".", "").Replace("-", "");
             var response = _customers.FirstOrDefault(customer => customer.Cpf == cpf);
             if (response is null) throw new ArgumentNullException($"Não foi encontrado Customer para o CPF: {cpf}");
             return response;
@@ -43,14 +50,12 @@ namespace DomainServices.Services
 
         public void Update(Customer model)
         {
-            int index = _customers.FindIndex(customer => customer.Id == model.Id);
-
-            if (index == -1) throw new ArgumentNullException($"Não foi encontrado Customer para o Id: {model.Id}");
-
-            if (_customers.Any(customer => customer.Email == model.Email && customer.Id != model.Id)) throw new ArgumentException($"Já existe usuário com o Email {model.Email}");
+            if (!_customers.Any(customer => customer.Id == model.Id)) throw new ArgumentNullException($"Não foi encontrado Customer para o Id: {model.Id}");
             if (_customers.Any(customer => customer.Cpf == model.Cpf && customer.Id != model.Id)) throw new ArgumentException($"Já existe usuário com o CPF {model.Cpf}");
+            if (_customers.Any(customer => customer.Email == model.Email && customer.Id != model.Id)) throw new ArgumentException($"Já existe usuário com o Email {model.Email}");
 
-            _customers[index] = model;
+            _customers.Update(model);
+            _context.SaveChanges();
         }
 
         public Customer GetById(long id)
@@ -61,16 +66,14 @@ namespace DomainServices.Services
             return response;
         }
 
-        public void UpdateEmail(long id, string email)
+        public void Modify(Customer model)
         {
-            int index = _customers.FindIndex(customer => customer.Id == id);
+            if (!_customers.Any(customer => customer.Id == model.Id)) throw new ArgumentNullException($"Não foi encontrado Customer para o Id: {model.Id}");
+            if (_customers.Any(customer => customer.Cpf == model.Cpf && customer.Id != model.Id)) throw new ArgumentException($"Já existe usuário com o CPF {model.Cpf}");
+            if (_customers.Any(customer => customer.Email == model.Email && customer.Id != model.Id)) throw new ArgumentException($"Já existe usuário com o Email {model.Email}");
 
-            if (index == -1) throw new ArgumentNullException($"Não foi encontrado Customer para o Id: {id}");
-
-            if (_customers.Any(customer => customer.Email == email)) throw new ArgumentException($"Já existe usuário com o E-mail digitado");
-
-            _customers[index].Id = id;
-            _customers[index].Email = email;
+            _context.Entry(model).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
         }
     }
 }
