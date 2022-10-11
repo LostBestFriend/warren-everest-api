5711615bc9f92347d6b4d28876e4220f46eeabbc
JoaoArrudaWarren <joao.arruda@warren.com.br>
2022-10-11 10:49:21 -0300

    [feat] First Tests

---
+++ ApiFirst/DomainServices.Tests/Services/CustomerServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Bogus;
+using Bogus.Extensions.Brazil;
+using DomainModels.Models;
+using Xunit;
+
+namespace DomainServices.Tests.Services
+{
+    public class CustomerServiceTest
+    {
+        [Fact]
+        public void Should_Create_SucessFully()
+        {
+            var customer = new Faker<Customer>("en_US").CustomInstantiator(
+                c => new Customer(
+                fullName: c.Name.FullName(),
+                email: c.Person.Email,
+                cpf: c.Person.Cpf(),
+                address: c.Address.FullAddress(),
+                country: c.Address.Country(),
+                city: c.Address.City(),
+                postalCode: c.Address.Locale,
+                whatsapp: c.PickRandom<bool>(),
+                emailSms: c.PickRandom<bool>(),
+                number: int.Parse(c.Address.BuildingNumber()),
+                cellphone: c.Phone.PhoneNumber(),
+                dateOfBirth: c.Person.DateOfBirth
+                )
+            );
+        }
+    }
+}
