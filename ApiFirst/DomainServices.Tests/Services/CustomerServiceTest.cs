d40a4e48fa562142bb67db921687bbbaf9a1f72a
JoaoArrudaWarren <joao.arruda@warren.com.br>
2022-10-13 11:25:30 -0300

    [feat] First Tests

--- ApiFirst/DomainServices.Tests/Services/CustomerServiceTest.cs
+++ ApiFirst/DomainServices.Tests/Services/CustomerServiceTest.cs
@@ -10,22 +10,21 @@ namespace DomainServices.Tests.Services
         [Fact]
         public void Should_Create_SucessFully()
         {
-            var customer = new Faker<Customer>("en_US").CustomInstantiator(
-                c => new Customer(
-                fullName: c.Name.FullName(),
-                email: c.Person.Email,
-                cpf: c.Person.Cpf(),
-                address: c.Address.FullAddress(),
-                country: c.Address.Country(),
-                city: c.Address.City(),
-                postalCode: c.Address.Locale,
-                whatsapp: c.PickRandom<bool>(),
-                emailSms: c.PickRandom<bool>(),
-                number: int.Parse(c.Address.BuildingNumber()),
-                cellphone: c.Phone.PhoneNumber(),
-                dateOfBirth: c.Person.DateOfBirth
-                )
-            );
+            var customer = new Faker<Customer>("en_US")
+                .RuleFor(p => p.Cpf, f => f.Person.Cpf())
+                .RuleFor(p => p.FullName, c => c.Name.FullName())
+                .RuleFor(p => p.Email, c => c.Person.Email)
+                .RuleFor(p => p.Address, c => c.Address.FullAddress())
+                .RuleFor(p => p.Country, c => c.Address.Country())
+                .RuleFor(p => p.City, c => c.Address.City())
+                .RuleFor(p => p.PostalCode, c => c.Address.Locale)
+                .RuleFor(p => p.Whatsapp, c => c.PickRandom<bool>())
+                .RuleFor(p => p.EmailSms, c => c.PickRandom<bool>())
+                .RuleFor(p => p.Number, c => int.Parse(c.Address.BuildingNumber()))
+                .RuleFor(p => p.Cellphone, c => c.Phone.PhoneNumber())
+                .RuleFor(p => p.DateOfBirth, c => c.Person.DateOfBirth);
+
+            customer.Generate();
         }
     }
 }
