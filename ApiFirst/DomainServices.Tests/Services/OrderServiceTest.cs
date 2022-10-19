b2a8c3cfccb40d57f584877e67d29929d7bca8be
JoaoArrudaWarren <joao.arruda@warren.com.br>
2022-10-19 13:46:27 -0300

    [feat] First Tests

--- ApiFirst/DomainServices.Tests/Services/OrderServiceTest.cs
+++ ApiFirst/DomainServices.Tests/Services/OrderServiceTest.cs
@@ -5,8 +5,12 @@ using EntityFrameworkCore.QueryBuilder.Interfaces;
 using EntityFrameworkCore.UnitOfWork.Interfaces;
 using FluentAssertions;
 using Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore.Query;
 using Moq;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using Xunit;
 
 namespace DomainServices.Tests.Services
@@ -57,6 +61,36 @@ namespace DomainServices.Tests.Services
             customers.Should().HaveCountGreaterThanOrEqualTo(0);
 
             _repositoryFactoryMock.Verify(p => p.Repository<Order>().Search(It.IsAny<IMultipleResultQuery<Order>>()), Times.Once);
+        }
+
+        [Fact]
+        public async void Should_GetByIdAsync_Sucessfully()
+        {
+            var order = OrderFixture.GenerateOrderFixture();
+            long id = 1;
+
+            _repositoryFactoryMock.Setup(p => p.Repository<Order>().SingleResultQuery().AndFilter(It.IsAny<Expression<Func<Order, bool>>>()).Include(It.IsAny<Func<IQueryable<Order>, IIncludableQueryable<Order, object>>>())).Returns(It.IsAny<IQuery<Order>>());
+            _repositoryFactoryMock.Setup(p => p.Repository<Order>().FirstOrDefaultAsync(It.IsAny<IQuery<Order>>(), default)).ReturnsAsync(order);
+
+            var result = await _orderService.GetByIdAsync(id);
+            result.Should().NotBeNull();
+
+            _repositoryFactoryMock.Verify(p => p.Repository<Order>().SingleResultQuery().AndFilter(It.IsAny<Expression<Func<Order, bool>>>()).Include(It.IsAny<Func<IQueryable<Order>, IIncludableQueryable<Order, object>>>()), Times.Once);
+            _repositoryFactoryMock.Verify(p => p.Repository<Order>().FirstOrDefaultAsync(It.IsAny<IQuery<Order>>(), default), Times.Once);
+        }
+
+        [Fact]
+        public void Should_GetExecutableOrders()
+        {
+            var orders = OrderFixture.GenerateOrderFixture(3);
+            _repositoryFactoryMock.Setup(p => p.Repository<Order>().MultipleResultQuery().AndFilter(It.IsAny<Expression<Func<Order, bool>>>())).Returns(It.IsAny<IQuery<Order>>());
+            _repositoryFactoryMock.Setup(p => p.Repository<Order>().Search(It.IsAny<IQuery<Order>>())).Returns(orders);
+
+            var result = _orderService.GetExecutableOrders();
+            result.Should().HaveCountGreaterThanOrEqualTo(0);
+
+            _repositoryFactoryMock.Verify(p => p.Repository<Order>().MultipleResultQuery().AndFilter(It.IsAny<Expression<Func<Order, bool>>>()), Times.Once);
+            _repositoryFactoryMock.Verify(p => p.Repository<Order>().Search(It.IsAny<IQuery<Order>>()), Times.Once);
         }
     }
 }
